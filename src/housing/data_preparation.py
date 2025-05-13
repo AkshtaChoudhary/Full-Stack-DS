ee4535d3e261311355feab538b16d779dee8a3c6
Akshta <akshta.choudhary@tigeranalytics.com>
2025-05-13 13:01:34 +0000

    Update data preparation for inferencing

--- src/housing/data_preparation.py
+++ src/housing/data_preparation.py
@@ -35,15 +35,57 @@ def stratified_split(data, testsize=0.2, splits=1):
 
 def prepare_data(data):
     data = data.copy()
+
+    # Feature engineering
     data["rooms_per_household"] = data["total_rooms"] / data["households"]
     data["bedrooms_per_room"] = data["total_bedrooms"] / data["total_rooms"]
     data["population_per_household"] = data["population"] / data["households"]
 
+    # Separate numeric columns
     num = data.drop("ocean_proximity", axis=1)
     imputer = SimpleImputer(strategy="median")
     num_prepared = pd.DataFrame(
-        imputer.fit_transform(num), columns=num.columns, index=num.index
+        imputer.fit_transform(num),
+        columns=num.columns,
+        index=data.index,
+    )
+    logging.info("Imputation of numeric columns complete.")
+
+    # Handle categorical variable
+    expected_categories = ["<1H OCEAN", "INLAND", "ISLAND", "NEAR BAY", "NEAR OCEAN"]
+    data["ocean_proximity"] = data["ocean_proximity"].astype(str)
+    data["ocean_proximity"] = pd.Categorical(
+        data["ocean_proximity"], categories=expected_categories, ordered=True
     )
 
-    cat = pd.get_dummies(data["ocean_proximity"], drop_first=True)
-    return num_prepared.join(cat), imputer
+    # Get dummy variables (drop_first=True to avoid dummy trap)
+    cat_encoded = pd.get_dummies(data["ocean_proximity"], drop_first=True)
+
+    # Ensure all expected dummy columns exist
+    expected_dummies = pd.get_dummies(
+        pd.Categorical(
+            expected_categories, categories=expected_categories, ordered=True
+        ),
+        drop_first=True,
+    ).columns
+
+    if (
+        len(list(set(cat_encoded.columns.tolist()) - set(expected_dummies.tolist())))
+        != 0
+    ):
+        for col in list(
+            set(expected_dummies.tolist()) - set(cat_encoded.columns.tolist())
+        ):
+            logging.info(f"Adding {col} as dummy variable")
+            cat_encoded[col] = 0
+
+    # Make sure column order is consistent
+    cat_encoded = cat_encoded[expected_dummies]
+
+    # Merging the dataframe
+    full_data = pd.concat(
+        [num_prepared, cat_encoded.set_index(num_prepared.index)], axis=1
+    )  #
+    logging.info("Dummy columns created for categorical variable.")
+
+    return full_data, imputer
